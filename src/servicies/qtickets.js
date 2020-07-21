0e1be5efd55bd3197b7c4f07d737540a42d13a50
sanya <donbobrino@gmail.com>
2020-07-21 20:23:09 +0300

    Cross Api testing

--- src/servicies/qtickets.js
+++ src/servicies/qtickets.js
@@ -1,62 +1,80 @@
-import axios from 'axios'
+import axios from "axios";
 
-let orderUrl = 'https://qtickets.ru/api/rest/v1/orders'
-let customersUrl = 'https://qtickets.ru/api/rest/v1/clients'
+let orderUrl = "https://qtickets.ru/api/rest/v1/orders";
+let customersUrl = "https://qtickets.ru/api/rest/v1/clients";
 
 let body = {
-    "data":{
-    "client": {
-      "email": "",
-      "details": {
-        "phone": "",
-        "name": "",
-        "middlename": "",
-        "surname": ""
-      }
+  site: {
+    host: "https://happy-bardeen-5a364c.netlify.app/",
+  },
+
+  payment_id: 6170,
+  payment_type_id: "card",
+  event_id: 10445,
+  currency_id: "RUB",
+  utm: {
+    utm_source: "google",
+    utm_medium: "cpc",
+    utm_campaign: "utm_metki",
+  },
+
+  fields: {
+    work_position: "Менеджер",
+    legal_name: 'ПАО "СИБУР ХОЛДИНГ"',
+    inn: "7727547261",
+    kpp: "720601001",
+  },
+};
+
+export const addNewClient = (user) => {
+  const baskets = [
+    {
+      show_id: 27637,
+      seat_id: "registraciya-na-gonku-1;1",
+      client_email: user.email,
+      client_phone: user.phone,
+      client_name: user.firstName,
+      client_surname: user.lastName,
+      work_position: "Менеджер",
+      legal_name: 'ПАО "СИБУР ХОЛДИНГ"',
+      inn: "7727547261",
+      kpp: "720601001",
     },
+  ];
 
-    "site": {
-      "host": "reserse.ru"
+  const client = {
+    email: user.email,
+    details: {
+      phone: user.phone,
+      name: user.firstName,
+      middlename: user.lastName,
+      surname: "",
     },
+  };
 
-    "payment_id": 6170,
-    "payment_type_id": "card",
-    "event_id": 10445,
-    "currency_id": "RUB",
-    "utm": {
-      "utm_source": "google",
-      "utm_medium": "cpc",
-      "utm_campaign": "utm_metki"
+  const custom = {
+    gen: user.gen,
+    distance: user.distance,
+    category: user.category,
+  };
+
+  const data = { data: { ...body, client, baskets, custom } };
+
+  const options = {
+    method: "POST",
+    headers: {
+      Accert: "application/json",
+      "Cache-Control": "no-cache",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.QTICKETS_API_KEY}`,
     },
-
-    "custom": {
-      "var1": "val1",
-      "var2": "val2"
-    },
-
-    "fields": {
-      "work_position": "Менеджер",
-      "legal_name": "ПАО \"СИБУР ХОЛДИНГ\"",
-      "inn": "7727547261",
-      "kpp": "720601001"
-    },
-
-    "baskets": [
-      {
-        "show_id": 27637,
-        "seat_id": "registraciya-na-gonku-1;1",
-        "client_email": "",
-        "client_phone": "",
-        "client_name": "",
-        "client_surname": "",
-        "work_position": "Менеджер",
-        "legal_name": "ПАО \"СИБУР ХОЛДИНГ\"",
-        "inn": "7727547261",
-        "kpp": "720601001"
-      }
-    ]
-  }
-}
+    // data: JSON.stringify(data),
+    url: orderUrl,
+  };
 
 
-export const addNewClient = (user) => {console.log('newYser', user)}
+
+  axios(options)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
