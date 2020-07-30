5dda79a3fa70dee12f38d2fd3bd2215b7ee210ac
sanya <donbobrino@gmail.com>
2020-07-30 11:17:41 +0300

    more tests

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Row = styled.div`
+  display: block;
   background: ${(props) =>
     props.invert ? "var(--main-color)" : "var(--secondary-color)"};
   padding-left: 12px;
@@ -14,31 +15,30 @@ const Row = styled.div`
   position: relative;
   overflow: hidden;
   max-width: 100%;
-
 `;
 
-const Label = styled.span`
-  z-index: 5;
-  position: absolute;
-  top: 0;
-  left: 15px;
-  border: none;
-  font-family: "Montserrat", sans-serif;
-  font-weight: 900;
-  color: var(--main-color);
-  font-size: var(--font-size-s);
-  pointer-events: none;
-  transition: 0.2s ease-in-out;
-  cursor: text;
-  white-space: nowrap;
-  text-transform: uppercase;
+// const Label = styled.span`
+//   z-index: 5;
+//   position: absolute;
+//   top: 0;
+//   left: 15px;
+//   border: none;
+//   font-family: "Montserrat", sans-serif;
+//   font-weight: 900;
+//   color: var(--main-color);
+//   font-size: var(--font-size-s);
+//   pointer-events: none;
+//   transition: 0.2s ease-in-out;
+//   cursor: text;
+//   white-space: nowrap;
+//   text-transform: uppercase;
 
-  @media (min-width: 1024px) {
-    top: 0;
-    left: 41px;
-    padding-top: 12px;
-  }
-`;
+//   @media (min-width: 1024px) {
+//     top: 0;
+//     left: 41px;
+//     padding-top: 12px;
+//   }
+// `;
 
 const OkButton = styled.button`
   display: block;
@@ -57,13 +57,14 @@ const OkButton = styled.button`
   font-family: "Montserrat", sans-serif;
   font-weight: 900;
   font-size: var(--font-size-xl);
-  /* min-height: 214px; */
-  max-width: 100%;
+  min-height: 214px;
   line-height: 100%;
   cursor: pointer;
   white-space: nowrap;
   z-index: 1;
   opacity: 1;
+  padding: 0;
+  margin: 0;
 
   &:disabled {
     -webkit-text-stroke: 4.5px var(--main-color);
@@ -91,7 +92,7 @@ const OkButton = styled.button`
 export default function Button({ type, title, caption, disabled, invert }) {
   return (
     <Row invert={invert}>
-      {title && <Label>{title}</Label>}
+      {/* {title && <Label>{title}</Label>} */}
       <OkButton type={type} disabled={disabled} invert={invert}>
         {caption}
       </OkButton>
