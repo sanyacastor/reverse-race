bd71bf127c960b99346f62500ec1e1ea4688ba3d
castor <donbobrino@gmail.com>
2020-07-31 12:28:27 +0300

    fixin

--- src/components/p5.js
+++ src/components/p5.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Loadable from "@loadable/component";
 import Letters from "./Letters.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: "600px";
+`;
 
 const P5Wrapper = Loadable(() => import("react-p5-wrapper"));
 
 const P5 = (props) => {
-  return <P5Wrapper sketch={Letters} />;
+  return  P5Wrapper ? <P5Wrapper sketch={Letters} /> : <Container>loading...</Container>;
 };
 
 export default P5;
