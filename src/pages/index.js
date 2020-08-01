2f9e9b00f99d8145aea5c6882890a125357c236e
castor <donbobrino@gmail.com>
2020-08-01 16:32:30 +0300

    000

--- src/pages/index.js
+++ src/pages/index.js
@@ -253,7 +253,9 @@ const FaqPage = () => {
       <SEO title='Reverse Race FAQ' />
       <Div100vh style={{height: '90rvh', minHeight: '90rvh', overflow: 'hidden'}}>
         <Header />
-        <P5 style={{height: '100vh', minHeight: '100vh', overflow: 'hidden'}} />
+        <Div100vh style={{height: '90rvh', minHeight: '90rvh', overflow: 'hidden'}}>
+          <P5 />
+        </Div100vh>
       </Div100vh>
       <SponsorBar>
         <Link
